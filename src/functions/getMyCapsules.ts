18b177f0b33a08eb4b082cb0a526e88fee9d8a4f
Abdo Ahmed <ba2662003@gmail.com>
2025-03-20 13:15:50 +0200

    update in get my capsules

--- src/functions/getMyCapsules.ts
+++ src/functions/getMyCapsules.ts
@@ -4,7 +4,7 @@ import { tableDataType } from "@/components/TableShowData"
 import { Dispatch, SetStateAction } from "react"
 
 export const getMyCapsules = (search:string,setData: Dispatch<SetStateAction<tableDataType | undefined>>)=>{
-    axiosInstance.get(`/capsule?search=${search}&limit=10`,{
+    axiosInstance.get(`/capsule?${search}&limit=10`,{
         headers:{
             Authorization:`Bearer ${cookie.get("token")}`,
             "Content-Type":"application/json"
